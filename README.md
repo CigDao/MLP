e7d33fb89202c6fd4e41529403f46a28ace0482d
Jonathan Green <dev.jonathan.green@gmail.com>
2023-01-08 02:20:19 -0500

    fixed issues with imports and config values

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 #TEST
 
 ```
-1. npm run build:link
-2. mlp -h
+1. npm install
+2. npm run build:link
+3. mlp new multi-sig
+4. mlp -h
 
 ```
--- src/multi-sig.ts
+++ src/multi-sig.ts
@@ -9,6 +9,7 @@ import { Command } from 'commander';
 import {execa} from 'execa';
 import { createSpinner } from 'nanospinner';
 import { Mlpconfig } from './init';
+import standard from 'figlet/importable-fonts/Standard.js'
 
 let dao_name: string;
 let member_principal: string[];
@@ -17,13 +18,13 @@ let token_principal = "token principal";
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 export default class MultiSig {
-    constructor(private program: Command, private config?: Mlpconfig){}
+    constructor(private program: Command, private config?: Mlpconfig) { }
 
     async welcome() {
         const rainbowTitle = chalkAnimation.rainbow(
             'Welcome To My Little Protocol \n'
         );
-    
+
         await sleep();
         rainbowTitle.stop();
     }
@@ -41,7 +42,7 @@ export default class MultiSig {
                     return 'dao name';
                 },
             });
-        
+
             dao_name = answers.dao_name;
         }
 
@@ -59,9 +60,9 @@ export default class MultiSig {
                     return 'principal';
                 },
             });
-        
+
             member_principal = answers.member_principal;
-    }
+        }
     }
 
     async install_dfx() {
@@ -71,7 +72,7 @@ export default class MultiSig {
             // Run external tool synchronously
             const installDfx = await execa("sh", ["-", "ci", `"$(curl -fsSL https://internetcomputer.org/install.sh)"`]);
             if (installDfx.exitCode !== 0) {
-                this.program.error("unable to install dfx, maybe install it manually?", {code: "1"})
+                this.program.error("unable to install dfx, maybe install it manually?", { code: "1" })
             };
             spinner.success({ text: `successfuly installed dfx`, mark: ':)' });
         }
@@ -80,25 +81,30 @@ export default class MultiSig {
 
     async deploy_multi_sig() {
         // Run external tool synchronously
-        const spinner = createSpinner('deploying canister...').start();
-
+        const spinner = createSpinner('deploying canister, this will take a few mins...').start();
+        let text = "("
+        let args = text.concat(`"${member_principal}",`, `"${token_principal}"`, ")");
         try {
-            const deploy = await execa("dfx", ["deploy", "--argument", `'("${member_principal}","${token_principal}")'`]);
+            const deploy = await execa("dfx", ["deploy", "--argument", args]);
             if (deploy.exitCode === 0) {
                 spinner.success({ text: `successfuly deployed canister`, mark: ':)' });
             }
 
-        }catch(e) {
+        } catch (e) {
             console.error(e);
+            this.program.error("failed to deploy canister", { code: "1" })
         }
-        this.program.error("failed to deploy canister", {code: "1"})
 
     }
 
     finish() {
-        figlet(`Congrats , ${dao_name} !\n Let's Make Crypto Great Again`, (err, data) => {
+        figlet.parseFont('Standard', standard);
+
+        figlet(`Congrats , ${dao_name} !\n Let's Make Crypto Great Again`, {
+            font: 'Standard',
+        }, (err, data) => {
             console.log(gradient.pastel.multiline(data) + '\n');
-    
+
             console.log(
                 chalk.green(
                     `Anyone can identify an opportunity and say "I could do that" or "that would be so easy", but ideas must go through a period of "execution" which kills the original idea and the real idea is born.`
